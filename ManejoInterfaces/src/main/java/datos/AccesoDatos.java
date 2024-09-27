b4d4383e77fa7289d344f504ec5aa0478d68b3fc
isidro <renplagnes@gmail.com>
2024-09-27 20:51:31 +0200

    interfaces

---
+++ ManejoInterfaces/src/main/java/datos/AccesoDatos.java
@@ -0,0 +1,11 @@
+package datos;
+
+public interface AccesoDatos {
+   public static final int MAX_REGISTROS = 10;
+   
+   public abstract void insertar();
+   
+   public abstract void listar();
+   
+    
+}
